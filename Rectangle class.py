f2d93151304bdb40a8b2982e7bc5659bc4f94177
carterchapman1 <carterroanchapman@gmail.com>
2025-10-08 11:38:01 +0100

    rectangle class thing

---
+++ Rectangle class.py
@@ -0,0 +1,20 @@
+import math
+
+class Rectangle:
+    def __innit__(self, len, height):
+        self.len = len
+        self.height = height
+    
+    def rectarea(self):
+        print(self.len * self.height)
+
+    def rectperimeter(self):
+        print(2*(self.len + self.height))
+
+    def rectdiagonal(self):
+        diag = math.sqrt(self.len^2+self.height^2)
+
+r1 = Rectangle(2,3)
+r1.rectdiagonal()
+r1.rectperimeter()
+r1.rectarea()
