b4f74bdb2b2fbe0f4ff8d75fd12ab6d0bdca009b
carterchapman1 <carterroanchapman@gmail.com>
2025-10-16 15:08:10 +0100

    implemented more

--- Vector class practice.py
+++ Vector class practice.py
@@ -14,18 +14,30 @@ class Vector:
     def __str__(self):
         return print(vector)
 
-   """ def __rmul__(self):
-        return Vector(self.magnitude, other)"""
+    def __mul__(self, scalar):
+        y = self.y * scalar
+        x = self.x * scalar
+        return (x,y)
+
+    def __rmul__(self, scalar):
+        return self * scalar
 
     def magnitude(self) -> float:
         return math.sqrt((self.x*self.x)+(self.y*self.y))
     
+    def __eq__(self, other):
+        equal : bool = self.x == other.x and self.y == other.y
+        return equal
+    
     def angle(self) -> float:
         return (180 / math.pi)*(math.atan(self.y/self.x))
+    def __str__(self):
+        return(f'X is {self.x} and Y is {self.y}!')
+    def test():
+        vector1 = Vector(3,4)
+        vector2 = Vector(6,7)
+        print(vector1)
+        print(vector2)
 
-vector = Vector(3,4)
-scalar = Scalar(1)
-print(vector.magnitude())
-print(vector.angle())
-
+Vector.test()
 
